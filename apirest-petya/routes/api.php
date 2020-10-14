56aee62e6bd006c0a6121f9c40be71f5f9968f76
oscarArdon <cdb5aardon4@gmail.com>
2020-10-14 14:03:04 -0600

    controlador cliente y formulario de citas

--- apirest-petya/routes/api.php
+++ apirest-petya/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//Declarando controladores
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\FormularioCitaController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//***********ruta para acceder al backend
+
+//ruta a metodos del controller cliente
+Route::resource('vetya-clientes', ClienteController::class);
+//ruta a metodos del controller cliente
+Route::resource('vetya-formcita', FormularioCitaController::class);
