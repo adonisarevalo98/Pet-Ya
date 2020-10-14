d78068104efa19ef646ce0beb9c8253ec51155f3
adonisarevalo98 <adonisarevalo503@gmail.com>
2020-10-14 14:19:45 -0600

    modificacion del index y login de prueba

--- petya-angular/src/app/app.module.ts
+++ petya-angular/src/app/app.module.ts
@@ -1,20 +1,53 @@
+import { FormsModule } from '@angular/forms';
+//firebase
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+//Service
+import { AuthService } from "./services/auth.service";
+import { AuthGuard } from "./guard/auth.guard";
+import { ToastrModule } from 'ngx-toastr';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './components/index/index.component';
+import { IndexComponent } from './index/index.component';
+
+
 
 @NgModule({
   declarations: [
+
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+   
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
