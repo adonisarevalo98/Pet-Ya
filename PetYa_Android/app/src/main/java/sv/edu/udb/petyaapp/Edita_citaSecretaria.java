8ebb797850a5cef471ab3b37f7373b18478358ed
Quevedo98 <oscarqp1@gmail.com>
2021-05-12 13:14:56 -0600

    1 cambios queve

---
+++ PetYa_Android/app/src/main/java/sv/edu/udb/petyaapp/Edita_citaSecretaria.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.petyaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edita_citaSecretaria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edita_cita_secretaria);
+    }
+}
